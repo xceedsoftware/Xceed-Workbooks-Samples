70566263320001a03dcccd97a85e0c365fce1061
Roberto vargas <robertovargashdez@gmail.com>
2024-07-23 16:38:15 -0400

    added code samples by platform

---
+++ Src/SamplesByPlatforms/Xceed.Blazor.Workbooks.Sample/Program.cs
@@ -0,0 +1,29 @@
+using Xceed.Blazor.Workbooks.Sample.Components;
+
+//Use a valid license key
+Xceed.Workbooks.NET.Licenser.LicenseKey = "XXXXX-YYYYY-ZZZZZ-XXXX";
+
+var builder = WebApplication.CreateBuilder( args );
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+	.AddInteractiveServerComponents();
+
+builder.Services.AddScoped<WorkBookCreator>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if( !app.Environment.IsDevelopment() )
+{
+	app.UseExceptionHandler( "/Error", createScopeForErrors: true );
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+	.AddInteractiveServerRenderMode();
+
+app.Run();
